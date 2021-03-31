6c0d61aeef65dcdc37dc7ead7345e2f1701613d8
balamut95 <47066323+Valentinv95sv@users.noreply.github.com>
2021-03-31 16:49:59 +0600

    complete

--- mysite/docParser/Docx_insert_data_class.py
+++ mysite/docParser/Docx_insert_data_class.py
@@ -1,28 +1,64 @@
+from docx.shared import Inches
 from mailmerge import MailMerge
 import datetime
+import docx
 
 class docx_insert_data_class:
 
-    def __init__(self, doc_path, inserted_data):
-        self.doc_path = doc_path
-        self.inserted_data = inserted_data
+    #метод, генерирующий название нового сгенерированного файла
+    def get_file_name(self):
+        filename = 'Blank_' + str(datetime.datetime.now().strftime("%H%M%S")) + '.docx'
+        return filename
 
-    def get_new_file(self, filename):
-        dict = self.inserted_data
-        template = self.doc_path
+    #метод, возвращиющий все специальные поля в документе
+    def get_fields(self, doc_path):
+        template = doc_path
         document = MailMerge(template)
+        return document.get_merge_fields()
+
+    #метод по добавлению данных только в поля
+    def merge_file_fields(self, doc_path, inserted_fields, filename):
+        dict = inserted_fields
+        document = MailMerge(doc_path)
         document.merge_pages([dict])
         document.write(filename)
         return filename
 
-    def get_fields(self):
-        template = self.doc_path
-        document = MailMerge(template)
-        return document.get_merge_fields()
-
-    def get_file_name(self):
-        filename = 'Blank_' + str(datetime.datetime.now().strftime("%H%M%S")) + '.docx'
+    #метод по добавлению данных только в таблицы
+    def merge_file_tables(self, doc_path, inserted_tables, filename):
+        document = MailMerge(doc_path)
+        for i in inserted_tables:
+            for j in i:
+                a = list(j.keys())[1]
+            document.merge_rows(a, i)
+        document.write(filename)
         return filename
+
+    #метод по добавлению данных в поля и поля таблиц, размеченных в документе
+    def merge_file_fields_tables(self, doc_path, inserted_fields, inserted_tables2, filename):
+        document = MailMerge(doc_path)
+        for i in inserted_tables2:
+            for j in i:
+                a = list(j.keys())[0]
+            document.merge_rows(a, i)
+        document.merge_pages([inserted_fields])
+        document.write(filename)
+        return filename
+
+#метод по добавлению изображений в формате png в поля таблиц, содержащих определенные символы
+    def add_picture(self, docpath, picPath):
+        doc = docx.Document(docpath)
+
+        for table in doc.tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    for paragraph in cell.paragraphs:
+                        for key, value in picPath.items():
+                            if ( paragraph.text == key):
+                                paragraph.text = paragraph.text.replace(key, "")
+                                a = paragraph.add_run()
+                                a.add_picture(value, width=Inches(0.7), height=Inches(0.5))
+        doc.save(docpath)
 
 
 
--- mysite/docParser/views.py
+++ mysite/docParser/views.py
@@ -3,23 +3,170 @@ from django.shortcuts import HttpResponse
 from django.shortcuts import render
 from . import Docx_insert_data_class
 
-inserted_data = {
-    'toWhom' : '123',
-    'fromWho' : '234',
-    'Tender_Number' : 'nony',
-    'max_work' : 'qwerrty'
+inserted_fields = {
+    'name/number_of_tender' : 'Supply tender AOA',
+    'day.month_of_signed' : '22.04',
+    'name_of_tender' : 'new tender of 2020',
+    'time_of_term' : '11:30',
+    'day_of_term': '23',
+    'month_of_term':'апреля'
 }
-template = 'docParser/Doc_form.docx'
+
+insert_table = [
+    {
+    'position1': 'Boss',
+    'full_name1':'Valentin V'
+    },
+    {
+    'position1': 'intern',
+    'full_name1':'Andrey'
+    }
+]
+insert_table2 = [
+    {
+        'TRU_name1':'only',
+        'Small_describe1':'bad',
+        'netto1':'3000'
+    },
+    {
+        'TRU_name1':'society',
+        'Small_describe1':'Good',
+        'netto1':'4500'
+    }
+]
+
+insert_table3 = [
+    {
+        'suppliers_name':'Valentin',
+        'suppliers_location':'Almaty',
+        'time_of_bid':'12.12.1998',
+        'lots_in_bid':'android, ios'
+    },
+    {
+        'suppliers_name':'Michail',
+        'suppliers_location':'Piter',
+        'time_of_bid':'12.12.1992',
+        'lots_in_bid':'android, windows'
+    }
+]
+
+insert_table4 = [
+    {
+        'describe_of_product':'good phone',
+        'Final_price':'199.999'
+    },
+    {
+        'describe_of_product':'bad phone',
+        'Final_price':'150.999'
+    },
+    {
+        'describe_of_product':'blue phone',
+        'Final_price':'19.999'
+    },
+    {
+        'describe_of_product':'red phone',
+        'Final_price':'200.999'
+    },
+    {
+        'describe_of_product':'dreen phone',
+        'Final_price':'9.999'
+    }
+]
+insert_table5 = [
+    {
+        'p/p_number':'12',
+        'supplier':'Andrey',
+        'rreason_for_rejection':'not enough docs'
+    },
+{
+        'p/p_number':'2',
+        'supplier':'Sven',
+        'rreason_for_rejection':'Bad name'
+    }
+]
+
+insert_table6 = [
+    {
+        'TRU_name2':'test',
+        'describ2e':'only letters',
+        'netto2':'676'
+    },
+    {
+        'TRU_name2':'wow!',
+        'describ2e':'many letters',
+        'netto2':'16'
+    }
+]
+
+insert_table7 = [
+    {
+        'TRU_name3':'qwerty',
+        'describe3':'lots of pain',
+        'netto3':'123'
+    },
+    {
+        'TRU_name3':'ios',
+        'describe3':'lots of pain',
+        'netto3':'23'
+    }
+]
+
+insert_table8 = [
+    {
+        'position2':'dj',
+        'full_name2':'david guetta'
+    },
+{
+        'position2':'dj',
+        'full_name2':'david guetta'
+    },
+{
+        'position2':'dj',
+        'full_name2':'david guetta'
+    },
+{
+        'position2':'dj',
+        'full_name2':'david guetta'
+    },
+{
+        'position2':'dj',
+        'full_name2':'david guetta'
+    },
+{
+        'position2':'dj',
+        'full_name2':'david guetta'
+    },
+]
+
+
+template = 'docParser/Shablon/ПИ ТЕНДЕР (ШАБЛОН)#.docx'
 path = 'blanks/'
-a = Docx_insert_data_class.docx_insert_data_class(template, inserted_data)
+
+a = Docx_insert_data_class.docx_insert_data_class()
+
+income = []
+income.append(insert_table)
+income.append(insert_table2)
+income.append(insert_table3)
+income.append(insert_table4)
+income.append(insert_table5)
+income.append(insert_table6)
+income.append(insert_table7)
+income.append(insert_table8)
+
+pict_path = {'david guetta': r'C:/MyWorkFlow/123.png', 'we': r'C:/MyWorkFlow/234.png'}
 
 def index(request):
     return render(request, 'test.html')
 
 def download(request):
-    file_path = a.get_new_file(path + a.get_file_name())
+    file_path = a.merge_file_fields_tables(template, inserted_fields, income, path + a.get_file_name())
+
+    a.add_picture(file_path, pict_path)
+
     if os.path.exists(file_path):
         with open(file_path, 'rb') as fh:
             response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
             response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
             return response
+
